dc8bb78d0a65364179307e775bd762349053ed36
Shyam Dorai Malla <shyam.dorai@sitero.com>
2025-01-03 20:27:25 +0530

    variable syntax changed

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSON1="Shyam"
 PERSON2="Chandini"
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hello $PERSON1"
+echo "${PERSON2}:: Hello $PERSON1"
 echo "$PERSON1:: How are you?"
 echo "$PERSON2:: I am fine, thank you"
